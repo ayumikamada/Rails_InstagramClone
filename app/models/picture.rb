9851d4503b9ea69dc0709b2f27be88cd1f35d696
ayumi kamada <dmrdmr58@gmail.com>
2020-09-10 07:04:05 +0900

    css等フロント部分修正

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -6,4 +6,5 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  THUMBNAIL_SIZE = [700, 700]
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,13 +1,17 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_limit: [700, 700]
+  version :thumb do 
+    process resize_to_fit: [100, 100] 
+  end 
+ # process resize_to_limit: [500, 500]
   storage :file
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  def picture_size  
+  def picture_size
     if picture.size > 5.megabytes  
       errors.add(:picture, "should be less than 5MB")  
     end
   end
+  
 end
