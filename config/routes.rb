8d0232f725a6515a853bd41d8e665ec407af9cdf
ayumi kamada <dmrdmr58@gmail.com>
2020-09-10 21:29:24 +0900

    TOPページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'tops#index'
   resources :contacts
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
