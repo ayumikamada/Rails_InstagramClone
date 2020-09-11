d8a07fa4647f7a7709a94dd41b66e6542c674592
ayumi kamada <dmrdmr58@gmail.com>
2020-09-11 21:14:25 +0900

    新規登録時のルート修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
       render :new
@@ -37,7 +38,7 @@ class UsersController < ApplicationController
   end
   def login_check
     unless logged_in?
-      redirect_to sessions_path
+      redirect_to new_session_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'tops#index'
-  resources :contacts
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :favorites, only: [:index, :create, :destroy]
@@ -9,5 +8,6 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+  resources :contacts
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
