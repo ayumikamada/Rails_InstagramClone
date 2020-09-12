33cfdc5c41fbe98ec5eaa977452192c3d188f41c
ayumi kamada <dmrdmr58@gmail.com>
2020-09-12 13:34:53 +0900

    Mailerの設定修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,17 +21,17 @@ Rails.application.configure do
   end
 
   config.active_record.dump_schema_after_migration = false
-end
 
-config.action_mailer.default_url_options = { host: 'shielded-cliffs-34872.herokuapp.com' }
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings =
-{
- user_name: ENV['SENDGRID_USERNAME'],
- password: ENV['SENDGRID_PASSWORD'],
- domain: "heroku.com",
- address: "smtp.sendgrid.net",
- port: 587,
- authentication: :plain,
- enable_starttls_auto: true
-}
+  config.action_mailer.default_url_options = { host: 'shielded-cliffs-34872.herokuapp.com' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: "heroku.com",
+  address: "smtp.sendgrid.net",
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+  }
+end
